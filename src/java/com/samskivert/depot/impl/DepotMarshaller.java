4a552b29bcb86786bb46e30210a27b627a955f3c
Par Winzell <zell@threerings.net>
2009-09-22 21:33:42 +0000

    Let's not run this MySQL atrocity unless absolutely necessary.

--- src/java/com/samskivert/depot/impl/DepotMarshaller.java
+++ src/java/com/samskivert/depot/impl/DepotMarshaller.java
@@ -765,8 +765,9 @@ public class DepotMarshaller<T extends PersistentRecord>
             // TIMESTAMP columns a value of "0000-00-00 00:00:00" regardless of whether we
             // explicitly provide a "DEFAULT" value for the column or not, and DATETIME columns
             // cannot accept CURRENT_TIME or NOW() defaults at all.
-            if (!coldef.nullable && (coldef.type.equalsIgnoreCase("timestamp") ||
-                                     coldef.type.equalsIgnoreCase("datetime"))) {
+            if (!coldef.nullable && coldef.defaultValue.length() == 0 &&
+                    (coldef.type.equalsIgnoreCase("timestamp") ||
+                     coldef.type.equalsIgnoreCase("datetime"))) {
                 log.info("Assigning current time to " + fmarsh.getColumnName() + ".");
                 ctx.invoke(new Modifier.Simple() {
                     @Override protected String createQuery (DatabaseLiaison liaison) {
