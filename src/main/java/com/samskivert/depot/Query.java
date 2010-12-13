90b04e68b85e3e6b941baa34bffe5d67cf53980d
Michael Bayne <mdb@samskivert.com>
2010-12-13 17:50:50 +0000

    Prevent confusion if someone uses selectCount() (which returns int) on a grouped query. They should be using select(Funcs.count(Exps.literal("*"))) (which returns List<Integer>) in such cases.

--- src/main/java/com/samskivert/depot/Query.java
+++ src/main/java/com/samskivert/depot/Query.java
@@ -359,6 +359,7 @@ public class Query<T extends PersistentRecord>
      */
     public int selectCount ()
     {
+        checkState(_groupBy == null, "You surely mean to select(Funcs.count(Exps.literal(\"*\"))).");
         _fromOverride = new FromOverride(_pclass);
         return _repo.load(CountRecord.class, _cache, getClauseArray()).count;
     }
