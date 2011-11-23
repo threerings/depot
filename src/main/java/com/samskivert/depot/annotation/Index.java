fb0a5f2884b6655ad20a569aabe1114f77a7bde4
Michael Bayne <mdb@samskivert.com>
2011-11-23 03:06:14 +0000

    Improve the documentation here.

--- src/main/java/com/samskivert/depot/annotation/Index.java
+++ src/main/java/com/samskivert/depot/annotation/Index.java
@@ -17,20 +17,20 @@ import java.lang.annotation.Target;
 public @interface Index
 {
     /**
-     * Defines the name of the index.<p>
+     * Defines the name of the index.
      *
-     * If this annotation is on a field, an index is created for all fields annotated with this
-     * name in the order of the fields in the class.<p>
+     * <p>If this annotation is on a field, an index is created for all fields annotated with this
+     * name in the order of the fields in the class.</p>
      *
-     * If this is defined, a static method must be defined on the record that provides the index
-     * configuration. The method must match one of the following two signatures:
+     * <p>If this annotation is on a {@code PersistentRecord} class, a static method must be
+     * defined in that class that provides the index configuration. The method must match one of
+     * the following two signatures:
      * <pre>
      * public static ColumnExp<?>[] indexName ()
      * public static List&lt;Tuple&lt;SQLExpression, OrderBy.Order>> indexName ()
      * </pre>
-     * The first form will result in a simple multicolum index being created
-     * with the supplied columns. The second will create a function index using the supplied
-     * SQLExpressions.
+     * The first form will result in a simple multicolum index being created with the supplied
+     * columns. The second will create a function index using the supplied {@code SQLExpression}s.</p>
      */
     String name () default "";
 
